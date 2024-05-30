23d5a9bb83a23a2f4e9076bcfb81e132a5fa4c66
YouGuess21 <fakeperson210904@gmail.com>
2024-05-31 03:30:34 +0530

    added sensors and signals they don't work yet.

--- trains.py
+++ trains.py
@@ -82,6 +82,44 @@ class TRACK:
         for block in self.route:
             block_rect = pygame.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size)
             pygame.draw.rect(screen, self.color, block_rect)
+    
+
+    def to_set(self):
+        return set((block.x, block.y) for block in self.route)
+
+class SIGNAL:
+    def __init__(self,  pos) :
+        self.pos = pos
+        self.flag = False
+
+    def draw_signal(self):
+        sign_rect = pygame.Rect(self.pos.x * cell_size - 0.5 * cell_size, self.pos.y * cell_size - 0.5 * cell_size, 2 * cell_size, 2* cell_size)
+        pygame.draw.rect(screen, (0, 200, 0), sign_rect)
+    
+    def update_signal(self, flag):
+        if flag == False:
+            sign_rect = pygame.Rect(self.pos.x * cell_size - 0.5 * cell_size, self.pos.y * cell_size - 0.5 * cell_size, 2 * cell_size, 2* cell_size)
+            pygame.draw.rect(screen, (0, 200, 0), sign_rect)
+        else:
+            sign_rect = pygame.Rect(self.pos.x * cell_size - 0.5 * cell_size, self.pos.y * cell_size - 0.5 * cell_size, 2 * cell_size, 2* cell_size)
+            pygame.draw.rect(screen, (200, 0, 0), sign_rect)
+
+
+
+class SENSOR:
+    def __init__(self,  pos, ntype) :
+        self.pos = pos
+        self.type = ntype #ntype is sensor type: 0 is caution, 1 safety
+        self.flag = False
+
+    def draw_sensor(self):
+        sens_rect = pygame.Rect(self.pos.x * cell_size - 0.5 * cell_size, self.pos.y * cell_size - 0.5 * cell_size, 2 * cell_size, 2* cell_size)
+        pygame.draw.rect(screen, (235, 235, 235), sens_rect)
+
+        bor_rect = pygame.Rect(self.pos.x * cell_size - 0.5 * cell_size, self.pos.y * cell_size - 0.5 * cell_size, 2 * cell_size, 2* cell_size)
+        pygame.draw.rect(screen, (0, 0, 0), bor_rect, 1)  
+
+# 
 
 class TRAIN:
     def __init__(self, track, start_index, length=5):
@@ -101,7 +139,7 @@ class TRAIN:
 
 
     def move_train(self):
-        self.position_indices = [(index + 1) % len(self.track.route) for index in self.position_indices]
+        self.position_indices = [(index - 1) % len(self.track.route) for index in self.position_indices]
         self.body = [self.track.route[i] for i in self.position_indices]
 
 class MAIN:
@@ -110,47 +148,83 @@ class MAIN:
         self.track1 = TRACK(1)
         self.track2 = TRACK(2)
 
-        self.train0 = TRAIN(self.track0, start_index=0, length=4)  
-        self.train1 = TRAIN(self.track0, start_index=294, length=4)  
-        self.train2 = TRAIN(self.track0, start_index=580, length=4) 
-        self.train3 = TRAIN(self.track1, start_index=0, length=4)  
-        self.train4 = TRAIN(self.track1, start_index=315, length=4)  
-        self.train5 = TRAIN(self.track1, start_index=630, length=4)  
-        self.train6 = TRAIN(self.track1, start_index=945, length=4) 
-        self.train7 = TRAIN(self.track2, start_index=0, length=4) 
-        self.train8 = TRAIN(self.track2, start_index=288, length=4)  
-        self.train9 = TRAIN(self.track2, start_index=574, length=4)   
- 
+        self.intersections = self.find_intersections()
+
+        self.trains = []
+        self.sensors = []
+        self.signals = []
+
+        for track, start_index in [(self.track0, 0), (self.track0, 294), (self.track0, 580),
+                                   (self.track1, 0), (self.track1, 315), (self.track1, 630), (self.track1, 945),
+                                   (self.track2, 0), (self.track2, 288), (self.track2, 574)]:
+            self.trains.append(TRAIN(track, start_index))
+
+        caution_sensor_positions = [
+            (Vector2(260, 30), 0), (Vector2(270, 20), 0), (Vector2(225, 170), 0), (Vector2(205, 180), 0),
+            (Vector2(215, 160), 0), (Vector2(152, 30), 0), (Vector2(162, 40), 0), (Vector2(130, 180), 0),
+            (Vector2(150, 170), 0), (Vector2(140, 160), 0), (Vector2(340, 100), 0), (Vector2(330, 80), 0),
+            (Vector2(320, 90), 0), (Vector2(50, 30), 0), (Vector2(60, 40), 0), (Vector2(20, 140), 0),
+            (Vector2(40, 150), 0), (Vector2(30, 160), 0), (Vector2(83, 20), 0)
+        ]
+
+        for pos, sensor_type in caution_sensor_positions:
+            self.sensors.append(SENSOR(pos, sensor_type))
+        
+        signal_positions = [
+            (Vector2(260, 25)), (Vector2(275, 20)), (Vector2(227, 165)), (Vector2(202, 180)),
+            (Vector2(219, 158)), (Vector2(150, 25)), (Vector2(166, 42)), (Vector2(126, 182)),
+            (Vector2(150, 165)), (Vector2(145, 160)), (Vector2(341, 105)), (Vector2(335, 80)),
+            (Vector2(320, 86)), (Vector2(50, 26)), (Vector2(56, 41)), (Vector2(20, 136)),
+            (Vector2(40, 146)), (Vector2(26, 160)), (Vector2(86, 18))
+        ]
+        for pos in signal_positions:
+            self.signals.append(SIGNAL(pos))
+
     def draw_elements(self):
         self.track0.draw_tracks()
         self.track1.draw_tracks()
         self.track2.draw_tracks()
-        self.train0.draw_train()
-        self.train1.draw_train()
-        self.train2.draw_train()
-        self.train3.draw_train()
-        self.train4.draw_train()
-        self.train5.draw_train()
-        self.train6.draw_train()
-        self.train7.draw_train()
-        self.train8.draw_train()
-        self.train9.draw_train()
+
+        for sensor in self.sensors:
+            sensor.draw_sensor()
+
+        for train in self.trains:
+            train.draw_train()
+
+        for signal in self.signals:
+            signal.draw_signal()
+        
+        self.signals[1].update_signal(True)
+
+        for intersection in self.intersections:
+            intersect_rect = pygame.Rect(intersection[0] * cell_size, intersection[1] * cell_size, cell_size, cell_size)
+            pygame.draw.rect(screen, (255, 255, 255), intersect_rect)
+
+            border_rect = pygame.Rect(
+                intersection[0] * cell_size - 0.5 * cell_size - 5,
+                intersection[1] * cell_size - 0.5 * cell_size - 5,
+                cell_size * 2 + 10,
+                cell_size * 2 + 10
+            )
+            pygame.draw.rect(screen, (50, 150, 50), border_rect, 1)
 
     def update(self):
-        self.train0.move_train()
-        self.train1.move_train()
-        self.train2.move_train()
-        self.train3.move_train()
-        self.train4.move_train()
-        self.train5.move_train()
-        self.train6.move_train()
-        self.train7.move_train()
-        self.train8.move_train()
-        self.train9.move_train()
+        for train in self.trains:
+            train.move_train()
+
+
+    def find_intersections(self):
+        set0 = self.track0.to_set()
+        set1 = self.track1.to_set()
+        set2 = self.track2.to_set()
+
+        intersections = set0.intersection(set1).union(set0.intersection(set2)).union(set1.intersection(set2))
+        print(intersections)
+        return intersections
 
 clock = pygame.time.Clock()
 SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE, 150)
+pygame.time.set_timer(SCREEN_UPDATE, 15)
 
 main_game = MAIN()
 
@@ -165,4 +239,6 @@ while True:
     screen.fill(BG_COLOR)
     main_game.draw_elements()
     pygame.display.update()
+
+
     clock.tick(FPS)
